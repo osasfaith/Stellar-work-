45ef637871dddc073c332392b2006bcb7ee0fd45
CAESAR MCMXCVII <rubelu2017@gmail.com>
2026-06-19 13:34:37 +0100

    done

--- create-pr.cmd
+++ create-pr.cmd
@@ -1,3 +1,3 @@
 @echo off
 REM Open GitHub PR creation page for the newbranch
-start https://github.com/Douglas4tech/Stellar-work-/compare/newbranch
+start https://github.com/Stelllar-Works/Stellar-work-/pulls
